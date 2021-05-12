7ac991bb0aae0e4115f4e2756a3cd486239d2a0e
Song Chang suk <a.ch.cocktail@gmail.com>
2021-05-13 00:37:42 +0900

    as4

--- LAB4/wine-script.js
+++ LAB4/wine-script.js
@@ -2,12 +2,13 @@
 // report any errors that occur in the fetch operation
 // once the products have been successfully loaded and formatted as a JSON object
 // using response.json(), run the initialize() function
+let prod;
 fetch('products.json').then(function(response) {
     return response.json();
 }).then(function(json) {
     let products = json;
     initialize(products);
-    listen(products);
+    //listen(products);
 }).catch(function(err) {
     console.log('Fetch problem: ' + err.message);
 });
